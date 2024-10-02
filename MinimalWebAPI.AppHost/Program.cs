3cebb4af381158658d26520298bd77e19ff5b2eb
Bill Newton <william.newton@poolcorp.com>
2024-10-02 09:55:18 -0400

    Added Comment

--- MinimalWebAPI.AppHost/Program.cs
+++ MinimalWebAPI.AppHost/Program.cs
@@ -3,6 +3,7 @@ var builder = DistributedApplication.CreateBuilder(args);
 var cosmosDb = builder.AddAzureCosmosDB("cosmos")
     .RunAsEmulator().AddDatabase("todo");
 
+// This initializes the Cosmos DB database and containers for cosmos emulator.
 var cosmosInitializer = builder.AddProject<Projects.MinimalWebAPI_CosmosInitializer>("minimalwebapi-cosmosinitializer")
     .WithReference(cosmosDb);
 
